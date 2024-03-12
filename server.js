623071c59587df804ed648280aa506b5920e7f4d
Paris Woods <148830068+pwoods125@users.noreply.github.com>
2024-03-11 21:36:33 -0400

    Added-PORT

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 
 // Notes router
-const PORT = process.env.port || 3001;
+const PORT = process.env.PORT || 3001;
 const api = require('./routes/index');
 const app = express();
 
